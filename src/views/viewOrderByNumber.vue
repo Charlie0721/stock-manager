bf7993a9df8863de93065309d75710b08edf8786
Carlos Casas <carloscasasramirez@gmail.com>
2023-03-02 13:17:56 -0500

    changes

--- src/views/viewOrderByNumber.vue
+++ src/views/viewOrderByNumber.vue
@@ -1,72 +1,73 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title class="letter-color">
-          <img class="edit-image1" src="../images/images_app/logo_header.png" />
-          Pedido Comercial número {{ numb }}</ion-title
-        >
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content>
       <ion-card>
         <ion-card-header>
-          <ion-card-subtitle class="letter-color"></ion-card-subtitle>
-          <h2 class="letter-color">Pedido número {{ numb }}</h2>
+          <ion-card-subtitle ></ion-card-subtitle>
+          <h5 class="letter-color">{{ warehouseName }}</h5>
+          <h5 class="letter-color">Fecha: {{ date }} hora:{{ hour }}</h5>
+          <h5 class="letter-color">Pedido Nro. {{ numb }}</h5>
 
           <ion-card-title class="letter-color"
-            >CLIENTE: {{ name }} {{ lastName }}</ion-card-title
-          >
+            ><h5 class="letter-color">{{ name }} {{ lastName }}</h5>
+            <h5 class="letter-color">Nit/CC: {{ nit }}</h5>
+          </ion-card-title>
           <vue-qr :text="numb" :size="120"></vue-qr>
         </ion-card-header>
 
         <ion-card-content>
-          <h2 class="letter-color">PRODUCTOS:</h2>
+          <h3 class="letter-color">PRODUCTOS:</h3>
 
           <ion-list v-for="product in order" :key="product.id">
             <ion-item>
-              <ion-label>
-                <h2 class="letter-color">
+              <ion-label class="box">
+                <h4 class="letter-color">
                   {{ product.descripcion }}
-                </h2>
-              </ion-label>
-            </ion-item>
-            <ion-item>
-              <ion-label>
-                <h3 class="letter-color">Cantidad: {{ product.cantidad }}</h3>
-              </ion-label>
-            </ion-item>
-            <ion-item>
-              <ion-label
-                ><ion-text color="medium">
-                  <h3 class="letter-color">
-                    Precio: $
+                </h4>
+                <h4 class="letter-color">Cantidad: {{ product.cantidad }}</h4>
+                <ion-text color="medium">
+                  <h4 class="letter-color">
+                    Vr Unit: $
                     {{
                       new Intl.NumberFormat("de-DE").format(product.valorprod)
                     }}
-                  </h3>
-                </ion-text></ion-label
-              >
+                  </h4>
+                </ion-text>
+                <ion-text color="medium">
+                  <h4 class="letter-color">
+                    Vr Total: ${{
+                      new Intl.NumberFormat("de-DE").format(
+                        (totalValueProduct =
+                          product.valorprod * product.cantidad)
+                      )
+                    }}
+                  </h4>
+                </ion-text>
+              </ion-label>
             </ion-item>
           </ion-list>
         </ion-card-content>
         <ion-card-header>
           <ion-card-title>
-            <h3 class="letter-color">
+            <h4 class="letter-color">
               TOTAL: $ {{ new Intl.NumberFormat("de-DE").format(totalValue) }}
-            </h3></ion-card-title
+            </h4></ion-card-title
           ></ion-card-header
         >
       </ion-card>
+
+      <ion-button
+        color="mycolor"
+        class="btn-edit-product"
+        expand="full"
+        @click="print()"
+        v-if="state === true"
+        ><ion-icon :icon="i.printOutline"></ion-icon>Imprimir
+      </ion-button>
     </ion-content>
     <ion-footer collapse="fade">
       <ion-toolbar>
         <ion-title>
-          <!-- <ion-router-link color="secondary" href="/trade-orders">Volver</ion-router-link> -->
-          <!-- <ion-button expand="full" color="mycolor" @click="returnOrder()"
-            ><ion-icon :icon="i.arrowBackSharp"></ion-icon>Volver
-          </ion-button> -->
-          <!-- <ion-button router-link="/trade-orders" router-direction="back" >Volver</ion-button> -->
           <a class="letter-color" href="/trade-orders">Volver</a>
         </ion-title>
       </ion-toolbar>
@@ -79,9 +80,10 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import * as allIcons from "ionicons/icons";
+
 import {
   IonPage,
-  IonHeader,
+  //IonHeader,
   IonToolbar,
   IonTitle,
   IonCard,
@@ -95,6 +97,8 @@ import {
   IonLabel,
   IonText,
   IonFooter,
+  IonButton,
+  IonIcon,
 } from "@ionic/vue";
 import { TradeOrders } from "../services/tradeOrder";
 import vueQr from "vue-qr/src/packages/vue-qr.vue";
@@ -105,7 +109,7 @@ export default defineComponent({
   components: {
     vueQr,
     IonPage,
-    IonHeader,
+    //  IonHeader,
     IonToolbar,
     IonTitle,
     IonCard,
@@ -119,6 +123,8 @@ export default defineComponent({
     IonLabel,
     IonText,
     IonFooter,
+    IonButton,
+    IonIcon,
   },
   data() {
     return {
@@ -130,18 +136,32 @@ export default defineComponent({
       lastName: "" as string,
       totalValue: 0 as number,
       urlWebPage: "" as string,
+      state: true as boolean,
+      hour: "" as string,
+      date: "" as string,
+      nit: "" as string,
+      totalValueProduct: 0 as number,
+      warehouseName: "" as string,
+      seller: "" as string,
     };
   },
   mounted() {
     this.getOrderByNumber();
   },
   methods: {
-    // returnOrder() {
-    //   //this.$router.push("/trade-orders");
-    //   const ionRouter = useIonRouter();
-    //   ionRouter.navigate('/trade-orders', 'forward', 'replace')
-    // },
+    print() {
+      let section = document.getElementById("printDocument");
+      console.log(section);
+      if (!section) {
+        section = document.createElement("div");
+        section.id = "print";
+        document.body.appendChild(section);
+      }
 
+      section.innerHTML = "";
+      //section.appendChild(cloned);
+      window.print();
+    },
     async getOrderByNumber() {
       try {
         this.idalm = this.$route.params.idalmacen.toString();
@@ -155,6 +175,10 @@ export default defineComponent({
         this.name = this.order[0].nombres;
         this.lastName = this.order[0].apellidos;
         this.totalValue = this.order[0].valortotal;
+        this.hour = this.order[0].hora;
+        this.date = this.order[0].fecha;
+        this.nit = this.order[0].nit;
+        this.warehouseName=this.order[0].nomalmacen
       } catch (error) {
         console.log(error);
       }
