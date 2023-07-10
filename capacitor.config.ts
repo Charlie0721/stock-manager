6b365ac5bcf17cc78a1cab64a7caab963ef4feac
Carlos Casas <carloscasasramirez@gmail.com>
2023-07-10 15:46:17 -0500

    new name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'new-app-search-prices',
+  appName: 'stock-manager',
   webDir: 'dist',
   bundledWebRuntime: false
 };
